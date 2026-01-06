172c66c2573f0c3d012c670941e20975fe10713c
gcpaccori <ptic252@else.com.pe>
2026-01-06 11:20:31 -0500

    Change DB credentials and host in app.py
    Updated database connection credentials and host.

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ CORS(app)  # Habilita conexión con Vercel
 # CONFIGURACIÓN BASE DE DATOS (LAZY, NO EN STARTUP)
 # =====================================================
 DB_USER = os.getenv('DB_USER', 'root')
-DB_PASS = os.getenv('DB_PASS', '')
-DB_HOST = os.getenv('DB_HOST', 'localhost')
+DB_PASS = os.getenv('DB_PASS', 'sismapiscis2025')
+DB_HOST = os.getenv('DB_HOST', '37.60.226.53')
 DB_NAME = 'sismapiscis'
 DB_PORT = os.getenv('DB_PORT', '3306')
 
@@ -329,3 +329,4 @@ def get_system_health():
 # --- ARRANQUE ---
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
